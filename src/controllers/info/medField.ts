f963d6f2169c1af1d99e1dfefefa1eaecbc8bb6f
Arnfinn Hykkerud Steindal <arnfinn.steindal@gmail.com>
2022-06-29 20:22:33 +0200

    extract medfields from db (#282)
    * extract medfields from db
    
    * fix
    
    * cleanup
    
    Co-authored-by: yte0 <Yohannes.Tesfay@helse-nord.no>

--- src/controllers/info/medField.ts
+++ src/controllers/info/medField.ts
@@ -4,7 +4,35 @@ import { medField } from "../../models/info";
 export const medicalFields: RequestHandler = async (_, res) => {
   try {
     const rows = await medField();
-    res.json(rows);
+    const emptyArray: {
+      shortName?: string;
+      name?: string;
+      registers?: string[];
+    }[] = [];
+
+    const testvalue = rows.reduce((prevVal, currVal) => {
+      prevVal
+        .filter((val) => val.shortName === currVal.shortName)
+        .forEach(
+          (val) =>
+            (val.registers = [...(val.registers ?? []), currVal.registers])
+        );
+      const returnValue = prevVal.some(
+        (val) => val.shortName === currVal.shortName
+      )
+        ? []
+        : [
+            {
+              shortName: currVal.shortName,
+              name: currVal.name,
+              registers: [currVal.registers],
+            },
+          ];
+
+      return [...prevVal, ...returnValue];
+    }, emptyArray);
+
+    res.json(testvalue);
   } catch (err) {
     const typed_err = err as any;
     res.status(500).json({ message: typed_err.message });
--- src/models/info/medField.ts
+++ src/models/info/medField.ts
@@ -1,3 +1,20 @@
-import { medField, MedFieldInterface } from "../../addToDb/medfield";
+import db from "../../db";
 
-export default (): MedFieldInterface[] => medField;
+export interface MedFieldInterface {
+  shortName: string;
+  name: string;
+  registers: string;
+}
+
+const medField = (): Promise<MedFieldInterface[]> =>
+  db
+    .select(
+      "mf.name as shortName",
+      "mf.full_name as name",
+      "r.name as registers"
+    )
+    .from("registry_medfield as rmf")
+    .leftJoin("medfield as mf", "rmf.medfield_id", "mf.id")
+    .join("registry as r", "rmf.registry_id", "r.id")
+    .orderBy("mf.id");
+export default medField;
