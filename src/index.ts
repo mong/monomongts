4deba798cb18850ef280329ec1df4d3f1126a803
Arnfinn Hykkerud Steindal <arnfinn.steindal@gmail.com>
2022-08-08 15:18:08 +0200

    Remove legacy end-point (#281)

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,6 @@ import registerDataRouter from "./routes/data";
 //controller
 import * as Description from "./controllers/Description";
 import * as Indicator from "./controllers/Indicator";
-import * as Legacy from "./controllers/Legacy";
 import * as TuName from "./controllers/TuName";
 
 const PORT = process.env.PORT ?? 4000;
@@ -75,7 +74,6 @@ app.get("/", (_, res) =>
 
 app.get("/description", cache, Description.index);
 app.get("/indicator", cache, Indicator.index);
-app.get("/legacy", cache, Legacy.index);
 app.get("/tu_name", cache, TuName.index);
 
 // Auth routes for CMS
